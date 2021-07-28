095a001a5b8d4bc464781b46d19766c0e9408c84
Abhishek hazra <abhishekhazra007@gmail.com>
2021-07-29 00:47:47 +0530

    DB check added

--- google_sheets_to_postgres.py
+++ google_sheets_to_postgres.py
@@ -2,9 +2,12 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
 import os
-from sqlalchemy import create_engine
+import sqlalchemy as sa
+from sqlalchemy.engine import make_url
+from sqlalchemy.sql.expression import true
 from sqlalchemy.types import Integer, Text, String, DateTime
 import datetime
+
 
 class GoogleSheetHelper:
     """Helper class to pull data from googlesheets"""
@@ -33,26 +36,96 @@ class GoogleSheetHelper:
         return [sheet.title for sheet in available_sheets]
 
 
+def _set_url_database(url , databasename):
+    """creates url with provided DB for the engine """
+
+    url = make_url(url)
+    if hasattr(sa.engine, 'URL'):
+        res = sa.engine.URL.create(
+            drivername=url.drivername,
+            username=url.username,
+            password=url.password,
+            host=url.host,
+            port=url.port,
+            database=databasename,
+            query=url.query
+        )
+    else:  # SQLAlchemy <1.4
+        url.database = databasename
+        res = url
+
+    return res
+
+
+def database_exists(url:str,dbname:str)->bool:
+    """Checkes if the database exists and returns bool"""
+
+    query  =  f'SELECT 1 FROM pg_database WHERE datname="{dbname}"'
+    en = sa.create_engine(url)
+    with en.connect() as con:
+        r = con.scalar(query)
+    if r :
+        return True
+    return False
+
+
+def create_database(url,dbname):
+    sql = f'CREATE DATABASE "{dbname}"'
+    en = sa.create_engine(url,isolation_level="AUTOCOMMIT", echo=True)
+    with en.connect() as con:
+        con.execute(sql)
+
+
+
+
+
 
 
 def excecute():
+
     from settings import Spreadsheet_config as sp_config, 
     from settings import PostgresSQL_config as psql_config
+
     if psql_config.__dict__.get('database'):
         database =  psql_config.database
     else:
         database =  sp_config.spreadsheetName
-    # host = "localhost"
-    # port = 5432
-    # username = "postgres"
-    # password = "password123"
-    # database = "goanddo" 
-    # database = "postgres" 
-    db_uri = f"postgresql://{psql_config.username}:{psql_config.password}@{psql_config.host}:{psql_config.port}"
-    /{psql_config.database}
-    if not database_exists(db_uri,)
-    engine = create_engine(db_uri, echo=True)
+    
+    DBDRIVER = psql_config.__dict__.get("DRIVER","postgresql")
+    
+    uri = f"{DBDRIVER}://{psql_config.username}:{psql_config.password}@{psql_config.host}:{psql_config.port}"
+
+    if not database_exists(uri,database):
+        create_database()
+    
+    url_with_db = _set_url_database(uri,database)
+    
+    engine = sa.create_engine(db_uri, echo=True)
+
+
 
 if __name__ == "__main__":
     pass
 
+    def _set_url_database(url: sa.engine.url.URL, database):
+    """Set the database of an engine URL.
+
+    :param url: A SQLAlchemy engine URL.
+    :param database: New database to set.
+
+    """
+    if hasattr(sa.engine, 'URL'):
+        ret = sa.engine.URL.create(
+            drivername=url.drivername,
+            username=url.username,
+            password=url.password,
+            host=url.host,
+            port=url.port,
+            database=database,
+            query=url.query
+        )
+    else:  # SQLAlchemy <1.4
+        url.database = database
+        ret = url
+    assert ret.database == database, ret
+    return ret
--- settings.py
+++ settings.py
@@ -1,13 +1,18 @@
 import os
 
 class Spreadsheet_config:
+    
     credential_path = os.environ['JSON_CRED']
     spreadsheetName = os.environ['SPREADSHEET_NAME'] 
 
 class PostgresSQL_config:
+
+    DRIVER = "postgresql"  # Dont change this  
     host = "localhost"
     port = 5432
     username = "postgres"
     password = "password123"
-    # database:= None      '''[optional] uncommenct and specify .Note the DB name will automatically be fetched from spreadsheetName'''  
-    # database = "postgres" 
+
+    # [optional] uncommenct and specify .DB name will automatically be fetched from spreadsheetName
+    
+    # database:= None       
